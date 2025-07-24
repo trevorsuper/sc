0d607fd472e438d69b75e076ef02e35572618d02
trevorsuper <129561006+trevorsuper@users.noreply.github.com>
2025-07-24 19:40:20 -0400

    scripts

---
+++ pi5.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/sh
+# Raspberry Pi 5 setup script for 64-bit Desktop
+sudo apt remove firefox chromium orca thonny geany dillo lynx emacsen-common -y
+sudo apt autoremove -y
+sudo apt update && sudo apt upgrade -y
+sudo apt install neofetch -y
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+sudo curl -fsSLo /etc/apt/sources.list.d/brave-browser-release.sources https://brave-browser-apt-release.s3.brave.com/brave-browser.sources
+sudo apt update && sudo apt install brave-browser -y
+wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
+    | gpg --dearmor \
+    | sudo dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
+echo 'deb [arch=amd64,arm64 signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg] https://download.vscodium.com/debs vscodium main' \
+    | sudo tee /etc/apt/sources.list.d/vscodium.list
+sudo apt update && sudo apt install codium -y
