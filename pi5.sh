c797e1e107660d8e2922ef8579ed8df70e3fa59a
trevorsuper <129561006+trevorsuper@users.noreply.github.com>
2025-07-29 13:37:46 -0400

    curl install

--- pi5.sh
+++ pi5.sh
@@ -3,7 +3,7 @@
 sudo apt remove firefox chromium orca thonny geany dillo lynx emacsen-common -y
 sudo apt autoremove -y
 sudo apt update && sudo apt upgrade -y
-sudo apt install neofetch -y
+sudo apt install curl -y
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 sudo curl -fsSLo /etc/apt/sources.list.d/brave-browser-release.sources https://brave-browser-apt-release.s3.brave.com/brave-browser.sources
 sudo apt update && sudo apt install brave-browser -y
@@ -21,3 +21,4 @@ brave-browser --incognito
 EOF
 sudo chmod +x Brave.sh
 cd ~/
+
