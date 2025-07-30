b107fd3fc722c2728cc39c68dbd252ec3db02b10
trevorsuper <129561006+trevorsuper@users.noreply.github.com>
2025-07-29 22:52:01 -0400

    minisign install

--- pi5.sh
+++ pi5.sh
@@ -3,7 +3,7 @@
 sudo apt remove firefox chromium orca thonny geany dillo lynx emacsen-common -y
 sudo apt autoremove -y
 sudo apt update && sudo apt upgrade -y
-sudo apt install curl -y
+sudo apt install curl minisign -y
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 sudo curl -fsSLo /etc/apt/sources.list.d/brave-browser-release.sources https://brave-browser-apt-release.s3.brave.com/brave-browser.sources
 sudo apt update && sudo apt install brave-browser -y
