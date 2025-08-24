aefc0a7b43aa750651ab20d3d78c90ab47611a69
trevorsuper <129561006+TrevorSuper@users.noreply.github.com>
2025-08-24 11:07:14 -0400

    .

--- lmvm.sh
+++ lmvm.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 sudo apt remove yt-dlp hypnotix firefox firefox-locale-en mintchat thunderbird \
  thunderbird-locale-en libimobiledevice-utils ifuse usbmuxd ideviceinstaller \
- blueman bluetooth bluez-cups bluez-obexd bluez
+ blueman bluetooth bluez-cups bluez-obexd bluez -y
 curl -fsS https://dl.brave.com/install.sh | sh
-sudo apt autoremove -y && sudo apt update && sudo apt upgrade && sudo reboot
+sudo apt autoremove -y && sudo apt update && sudo apt upgrade -y && sudo reboot
