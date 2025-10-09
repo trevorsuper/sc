1ef98d0d4a4cc48f18b6dd71f4c70890a90c3f27
trevorsuper <129561006+TrevorSuper@users.noreply.github.com>
2025-10-09 16:16:13 -0400

    geany

--- pi5.sh
+++ pi5.sh
@@ -18,21 +18,14 @@ sudo systemctl restart NetworkManager
 
 sleep 5 # seconds
 # Package preferences
-sudo apt remove firefox chromium orca thonny dillo lynx emacsen-common geany -y
+sudo apt remove firefox chromium orca thonny dillo lynx emacsen-common -y
 sudo apt update && sudo apt upgrade -y
-sudo apt install curl rsync minisign -y
+sudo apt install curl rsync minisign geany -y
 sudo apt autoremove -y
 
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 sudo curl -fsSLo /etc/apt/sources.list.d/brave-browser-release.sources https://brave-browser-apt-release.s3.brave.com/brave-browser.sources
 sudo apt update && sudo apt install brave-browser -y
-
-wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
-    | gpg --dearmor \
-    | sudo dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
-echo 'deb [arch=amd64,arm64 signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg] https://download.vscodium.com/debs vscodium main' \
-    | sudo tee /etc/apt/sources.list.d/vscodium.list
-sudo apt update && sudo apt install codium -y
 
 cat << EOF > ~/Desktop/Brave.sh
 #!/usr/bin/sh
