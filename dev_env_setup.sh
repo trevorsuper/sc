8b03fa79621b50bf21e42de13eeb9a2f53fe836d
trevorsuper <129561006+trevorsuper@users.noreply.github.com>
2026-01-25 22:03:28 -0500

    Add meson

--- dev_env_setup.sh
+++ dev_env_setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/sh
 sudo apt install geany geany-plugin-codenav geany-plugin-scope \
  geany-plugin-treebrowser git curl rsync libcurl4-openssl-dev clang \
- libfuzzer-19-dev -y
+ libfuzzer-19-dev meson -y
 
 mkdir -p ~/.config/geany/colorschemes
 
