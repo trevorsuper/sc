923e786bb9afc2ed276a4c66e562739d0abfbed5
trevorsuper <129561006+trevorsuper@users.noreply.github.com>
2026-02-11 22:23:56 -0500

    Pahole and meson

--- linux_mint_setup.sh
+++ linux_mint_setup.sh
@@ -25,7 +25,7 @@ sudo apt autoremove -y
 sudo apt update && sudo apt upgrade -y
 sudo apt install git curl rsync xreader xed geany geany-plugin-codenav \
  geany-plugin-scope geany-plugin-treebrowser libimage-exiftool-perl \
- pavucontrol clang libfuzzer-19-dev libcurl4-openssl-dev -y
+ pavucontrol clang meson libfuzzer-19-dev libcurl4-openssl-dev pahole -y
 
 mkdir -p ~/.config/geany/colorschemes
 
