b2457ef520f67b7bf1b2aab0df7009cb23e3c74e
trevorsuper <129561006+TrevorSuper@users.noreply.github.com>
2026-02-19 22:25:31 -0500

    add set_dns_blocks.sh

---
+++ set_dns_blocks.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/sh
+curl -sSLo ~/hosts.tar.gz https://github.com/trevorsuper/sc/raw/refs/heads/master/files/hosts.tar.gz
+cd ~/
+expected_checksum="dff1b1ebb65897c5b6968bd11cff7a0db9ff67f967bf4b9daf927e1df07cfba7"
+if [ "$(sha256sum ~/hosts.tar.gz | awk '{ print $1 }')" = "$expected_checksum" ]; then
+    echo "hosts.tar.gz checksum is valid"
+    tar -xzf hosts.tar.gz
+    cat hosts | sudo tee -a /etc/hosts > /dev/null
+    rm hosts hosts.tar.gz
+else
+    echo ""
+    echo "==========================================================================="
+    echo ""
+    echo "hosts.tar.gz checksum is not valid"
+    echo ""
+    echo "==========================================================================="
+    echo ""
+    rm hosts.tar.gz
+fi
+
